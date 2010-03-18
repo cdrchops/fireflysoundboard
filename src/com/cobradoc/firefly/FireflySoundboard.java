a9778308af624c92caa078617ddfefaca5f490d8
standtrooper <standtrooper@11197922-c41c-cb7b-5a2f-8d80cf4fc496>
2010-03-18 05:02:26 +0000

    show the same display if you switch the orientation from portrait to landscape

--- src/com/cobradoc/firefly/FireflySoundboard.java
+++ src/com/cobradoc/firefly/FireflySoundboard.java
@@ -62,14 +62,13 @@ import com.cobradoc.firefly.board.*;
 public class FireflySoundboard extends Activity {
 
     private MediaPlayer player;
-
+    private static int playerValue = R.id.AllSoundsMenu;
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main);
-
-        createGrid(new AllSounds());
+        selection(playerValue);
     }
 
     private void createGrid(final Soundboard clazz) {
@@ -119,55 +118,72 @@ public class FireflySoundboard extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(final MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.about:
-                startActivity(new Intent(this, About.class));
-                return true;
-            case R.id.quit:
-                finish();
-                return true;
-            case R.id.AllSoundsMenu:
-                createGrid(new AllSounds());
-                return true;
-            case R.id.MalMenu:
-                createGrid(new Mal());
-                return true;
-            case R.id.ZoeMenu:
-                createGrid(new Zoe());
-                return true;
-            case R.id.JubalEarlyMenu:
-                createGrid(new JubalEarly());
-                return true;
-            case R.id.JayneMenu:
-                createGrid(new Jayne());
-                return true;
-            case R.id.WashMenu:
-                createGrid(new Wash());
-                return true;
-            case R.id.KayleeMenu:
-                createGrid(new Kaylee());
-                return true;
-            case R.id.RiverMenu:
-                createGrid(new River());
-                return true;
-            case R.id.SimonMenu:
-                createGrid(new Simon());
-                return true;
-            case R.id.BookMenu:
-                createGrid(new Book());
-                return true;
-//             case R.id.MrUniverseMenu:
-//                createGrid(new MrUniverse());
-//                 return true;
-            case R.id.InaraMenu:
-                createGrid(new Inara());
-                return true;
-//             case R.id.TheOperativeMenu:
-//                createGrid(new TheOperative());
-//                 return true;
-        }
+        return selection(item.getItemId());
+    }
+    
+    private boolean selection(final int id) {
+    	switch (id) {
+	        case R.id.about:
+	            startActivity(new Intent(this, About.class));
+	            return true;
+	        case R.id.quit:
+	            finish();
+	            return true;
+	        case R.id.AllSoundsMenu:
+	        	playerValue = R.id.AllSoundsMenu;
+	            createGrid(new AllSounds());
+	            return true;
+	        case R.id.MalMenu:
+                playerValue = R.id.MalMenu;
+	            createGrid(new Mal());
+	            return true;
+	        case R.id.ZoeMenu:
+                playerValue = R.id.ZoeMenu;
+	            createGrid(new Zoe());
+	            return true;
+	        case R.id.JubalEarlyMenu:
+                playerValue = R.id.JubalEarlyMenu;
+	            createGrid(new JubalEarly());
+	            return true;
+	        case R.id.JayneMenu:
+                playerValue = R.id.JayneMenu;
+	            createGrid(new Jayne());
+	            return true;
+	        case R.id.WashMenu:
+                playerValue = R.id.WashMenu;
+	            createGrid(new Wash());
+	            return true;
+	        case R.id.KayleeMenu:
+                playerValue = R.id.KayleeMenu;
+	            createGrid(new Kaylee());
+	            return true;
+	        case R.id.RiverMenu:
+                playerValue = R.id.RiverMenu;
+	            createGrid(new River());
+	            return true;
+	        case R.id.SimonMenu:
+                playerValue = R.id.SimonMenu;
+	            createGrid(new Simon());
+	            return true;
+	        case R.id.BookMenu:
+                playerValue = R.id.BookMenu;
+	            createGrid(new Book());
+	            return true;
+	//         case R.id.MrUniverseMenu:
+//            playerValue = R.id.MrUniverseMenu;
+	//            createGrid(new MrUniverse());
+	//             return true;
+	        case R.id.InaraMenu:
+                playerValue = R.id.InaraMenu;
+	            createGrid(new Inara());
+	            return true;
+	//         case R.id.TheOperativeMenu:
+//            playerValue = R.id.TheOperativeMenu;
+	//            createGrid(new TheOperative());
+	//             return true;
+	    }
 
-        return false;
+    	return false;
     }
 
 }
