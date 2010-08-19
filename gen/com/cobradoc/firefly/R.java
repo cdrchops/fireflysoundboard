289fad8fa487a6a4a875b94d847fc7e79e9e20f3
standtrooper@gmail.com <standtrooper@gmail.com@11197922-c41c-cb7b-5a2f-8d80cf4fc496>
2010-08-19 20:23:59 +0000

    Adding alert for beginning

--- gen/com/cobradoc/firefly/R.java
+++ gen/com/cobradoc/firefly/R.java
@@ -15,32 +15,34 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int AllSoundsMenu=0x7f060006;
-        public static final int BookMenu=0x7f06000f;
-        public static final int InaraMenu=0x7f060010;
-        public static final int JayneMenu=0x7f060009;
-        public static final int JubalEarlyMenu=0x7f06000b;
-        public static final int KayleeMenu=0x7f06000c;
+        public static final int AllSoundsMenu=0x7f060007;
+        public static final int BookMenu=0x7f060010;
+        public static final int InaraMenu=0x7f060011;
+        public static final int JayneMenu=0x7f06000a;
+        public static final int JubalEarlyMenu=0x7f06000c;
+        public static final int KayleeMenu=0x7f06000d;
         public static final int Mal=0x7f060003;
-        public static final int MalMenu=0x7f060007;
-        public static final int RiverMenu=0x7f06000d;
-        public static final int SimonMenu=0x7f06000e;
-        public static final int WashMenu=0x7f06000a;
-        public static final int ZoeMenu=0x7f060008;
+        public static final int MalMenu=0x7f060008;
+        public static final int RiverMenu=0x7f06000e;
+        public static final int SimonMenu=0x7f06000f;
+        public static final int WashMenu=0x7f06000b;
+        public static final int ZoeMenu=0x7f060009;
         public static final int about=0x7f060004;
+        public static final int alert=0x7f060005;
         public static final int app_name=0x7f060001;
         public static final int image=0x7f060000;
-        public static final int quit=0x7f060011;
-        public static final int settings=0x7f060005;
-        public static final int tab1=0x7f060015;
-        public static final int tab2=0x7f060016;
-        public static final int tab3=0x7f060017;
-        public static final int tab4=0x7f060018;
-        public static final int tab5=0x7f060019;
-        public static final int tab6=0x7f06001a;
-        public static final int tabbedButton=0x7f060013;
-        public static final int tabbedLayout=0x7f060012;
-        public static final int tabhost=0x7f060014;
+        public static final int quit=0x7f060012;
+        public static final int settings=0x7f060006;
+        public static final int tab1=0x7f060016;
+        public static final int tab2=0x7f060017;
+        public static final int tab3=0x7f060018;
+        public static final int tab4=0x7f060019;
+        public static final int tab5=0x7f06001a;
+        public static final int tab6=0x7f06001b;
+        public static final int tab7=0x7f06001c;
+        public static final int tabbedButton=0x7f060014;
+        public static final int tabbedLayout=0x7f060013;
+        public static final int tabhost=0x7f060015;
         public static final int text=0x7f060002;
     }
     public static final class layout {
@@ -107,26 +109,27 @@ public final class R {
         public static final int worry=0x7f040034;
     }
     public static final class string {
-        public static final int AllSounds=0x7f050012;
-        public static final int Book=0x7f050010;
-        public static final int Inara=0x7f05000f;
-        public static final int Jayne=0x7f050008;
-        public static final int JubalEarly=0x7f050009;
-        public static final int Kaylee=0x7f05000b;
-        public static final int Mal=0x7f050006;
-        public static final int MrUniverse=0x7f050011;
-        public static final int River=0x7f05000d;
-        public static final int Simon=0x7f05000c;
-        public static final int TheOperative=0x7f05000e;
-        public static final int Wash=0x7f05000a;
-        public static final int Zoe=0x7f050007;
+        public static final int AllSounds=0x7f050013;
+        public static final int Book=0x7f050011;
+        public static final int Inara=0x7f050010;
+        public static final int Jayne=0x7f050009;
+        public static final int JubalEarly=0x7f05000a;
+        public static final int Kaylee=0x7f05000c;
+        public static final int Mal=0x7f050007;
+        public static final int MrUniverse=0x7f050012;
+        public static final int River=0x7f05000e;
+        public static final int Simon=0x7f05000d;
+        public static final int TheOperative=0x7f05000f;
+        public static final int Wash=0x7f05000b;
+        public static final int Zoe=0x7f050008;
         public static final int about_date=0x7f050002;
         public static final int about_label=0x7f050004;
         public static final int about_title=0x7f050003;
+        public static final int alert_label=0x7f050005;
         public static final int app_name=0x7f050000;
         public static final int author=0x7f050001;
-        public static final int quit_label=0x7f050005;
-        public static final int settings_label=0x7f050013;
-        public static final int settings_title=0x7f050014;
+        public static final int quit_label=0x7f050006;
+        public static final int settings_label=0x7f050014;
+        public static final int settings_title=0x7f050015;
     }
 }
